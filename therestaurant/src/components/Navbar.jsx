04a76fa123544bc9d4d74ff7ba4802824ba6f188
Ahmed Abbasi <contact@ahmed.dev>
2024-04-03 20:45:48 +0200

    Add active state for NavLinks

--- therestaurant/src/components/Navbar.jsx
+++ therestaurant/src/components/Navbar.jsx
@@ -1,20 +1,34 @@
 import { NavLink } from "react-router-dom";
 
-export const Navbar = () => (
-    <nav>
-        <ul>
-            <li>
-                <NavLink to={"/"}>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to={"/booking"}>Booking</NavLink>
-            </li>
-            <li>
-                <NavLink to={"/contact"}>Contact</NavLink>
-            </li>
-            <li>
-                <NavLink to={"/admin"}>Admin</NavLink>
-            </li>
-        </ul>
-    </nav>
-);
+export const Navbar = () => {
+    const linkState = ({ isActive }) => ({
+        textDecorationColor: isActive && "#242525",
+    });
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to={"/"} style={linkState}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/booking"} style={linkState}>
+                        Booking
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/contact"} style={linkState}>
+                        Contact
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/admin"} style={linkState}>
+                        Admin
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
