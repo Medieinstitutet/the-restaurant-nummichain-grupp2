23e828ed431d5545bfdcdff10bc90fdaf0adbb4e
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2024-04-05 09:42:40 +0200

    added email+ submitHandler

--- therestaurant/src/components/Admin/Login.jsx
+++ therestaurant/src/components/Admin/Login.jsx
@@ -1,14 +1,29 @@
 
-
+import { useState } from "react";
 export default function Login() {
+    
+    const[enteredEmail, setEnteredEmail]=useState('');
+//  const [enteredPassword, setEnteredPassword]= useState('');
+
+const submitHandeler=(event)=>{
+    event.preventDefault();
+  console.log('user Email:'+ enteredEmail) ;
+}
+ const emailChangeHandeler=(event)=>{
+    setEnteredEmail(event.target.value);
+   
+ }
+
+
+    
     return (
-      <form className="adminform">
+      <form className="adminform" onSubmit={submitHandeler}>
         <h2>Login</h2>
   
         <div className="control-row">
           <div className="control no-margin">
             <label htmlFor="email">Email</label>
-            <input id="email" type="email" name="email" />
+            <input id="email" type="email" name="email" onChange={emailChangeHandeler} value={enteredEmail}/>
           </div>
   
           <div className="control no-margin">
