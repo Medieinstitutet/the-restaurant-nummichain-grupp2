0bbd308663fe8035f9640a9ce4ae6cff20126c11
0xMayo <wakjiraalemayehu@gmail.com>
2024-04-07 00:43:01 +0200

    Created a RestaurantCreate component so we can activate our restaurant.

--- therestaurant/src/pages/Admin.jsx
+++ therestaurant/src/pages/Admin.jsx
@@ -1,7 +1,12 @@
+
+import React from 'react';
+import RestaurantCreator from '../components/RestaurantCreator';
+
 export const Admin = () => {
     return (
         <>
         <h1>Admin</h1>
+        <RestaurantCreator />
         </>
     );
   }
