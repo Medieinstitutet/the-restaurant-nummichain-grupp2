eb7c8dd3efe42bb648a339f1e60591be9bf5dea5
Ahmed Abbasi <contact@ahmed.dev>
2024-04-10 04:52:59 +0200

    Enhance Admin interface styling
    Co-authored-by: Elena Fredenbrink <elena.fredenbrink@gmail.com>

--- therestaurant/src/components/Admin/AdminBookings.jsx
+++ therestaurant/src/components/Admin/AdminBookings.jsx
@@ -1,4 +1,4 @@
-import FormManagement from'./FormManagement'
+import FormManagement from "./FormManagement";
 import { useState, useEffect } from "react";
 import { useContracts } from "../../hooks/useContract";
 
@@ -9,193 +9,190 @@ import { useBookingSubmission } from "../../hooks/useBookingHandleSubmition";
 import useBookingEditor from "../../hooks/useBookingEditor";
 
 import BookingsListA from "./BookingListA";
-import FilterSection from './FilterSectionA.jsx';
-
+import FilterSection from "./FilterSectionA.jsx";
 
 import { timeSlotMapping } from "../../utils/timeSlot";
 
 const AdminInterface = () => {
-  const [readContract, writeContract] = useContracts();
-  const { bookings, createBooking, editBooking, removeBooking } =
-    useBookingManagement(readContract, writeContract);
+    const [readContract, writeContract] = useContracts();
+    const { bookings, createBooking, editBooking, removeBooking } =
+        useBookingManagement(readContract, writeContract);
 
-  const [guests, setGuests] = useState("1");
-  const [name, setName] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("18:00 - 21:00");
+    const [guests, setGuests] = useState("1");
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("18:00 - 21:00");
 
+    const [selectedDate, setSelectedDate] = useState("");
+    const [selectedTimeSlot, setSelectedTimeSlot] = useState("");
+    const [searchBookings, setSearchBookings] = useState("");
 
-  const [selectedDate, setSelectedDate] = useState("");
-  const [selectedTimeSlot, setSelectedTimeSlot] = useState("");
-  const [searchBookings, setSearchBookings] = useState("");
+    const today = new Date().toISOString().split("T")[0];
+    const [tableAvailabilityMessage, setTableAvailabilityMessage] =
+        useState("");
+    const restaurantID = 1;
 
-  const today = new Date().toISOString().split("T")[0];
-  const [tableAvailabilityMessage, setTableAvailabilityMessage] = useState("");
-  const restaurantID = 1;
+    const { guestCount, tablesBookedByCustomer, availableTables } =
+        useGuestAndTableCount(bookings, selectedDate, selectedTimeSlot);
 
-  const { guestCount, tablesBookedByCustomer, availableTables } =
-    useGuestAndTableCount(bookings, selectedDate, selectedTimeSlot);
-
-  const {
-    isEditing,
-    editingBookingId,
-    startEditBooking,
-    stopEditing,
-    hasBookingChanged,
-    setIsEditing,
-    setEditingBookingId,
-  } = useBookingEditor({
-    setGuests,
-    setName,
-    setDate,
-    setTime,
-  });
-
-  const submitBooking = useBookingSubmission({
-    createBooking,
-    editBooking,
-    availableTables,
-    setGuests,
-    setName,
-    setDate,
-    setTime,
-    setIsEditing,
-    setEditingBookingId,
-    hasBookingChanged,
-    restaurantID,
-  });
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      // Call submitBooking with form data, handling both create and edit operations
-      await submitBooking(event, {
-        guests,
-        name,
-        date,
-        time,
+    const {
         isEditing,
         editingBookingId,
-      });
-    } catch (error) {
-   
-      console.error("Failed to process booking:", error);
-    }
-  };
+        startEditBooking,
+        stopEditing,
+        hasBookingChanged,
+        setIsEditing,
+        setEditingBookingId,
+    } = useBookingEditor({
+        setGuests,
+        setName,
+        setDate,
+        setTime,
+    });
 
-  const handleRemoveBooking = async (id) => {
-    await removeBooking(id);
-  };
-
-  // const handleChangeInSearch = (e) => {
-  //   setSearchBookings(e.target.value);
-  // };
-
-  const resetFilters = () => {
-    setSelectedDate("");
-    setSelectedTimeSlot("");
-    setSearchBookings("");
-  };
-
-  useEffect(() => {
-    if (selectedDate && selectedTimeSlot) {
-      console.log("Initial bookings:", bookings);
-
-      console.log("Guest Count:", guestCount);
-      console.log("Tables Booked by Customer:", tablesBookedByCustomer);
-
-      const totalBookedTables = Object.values(tablesBookedByCustomer).reduce(
-        (acc, cur) => acc + cur,
-        0,
-      );
-      console.log("Total Booked Tables:", totalBookedTables);
-
-               // Log for debugging purposes
-          console.log("Available Tables (inside useEffect):", availableTables);
-      
-          setTableAvailabilityMessage(`Available Tables: ${availableTables}`);
-        } else {
-          setTableAvailabilityMessage(
-            "Please select a date and time to check table availability."
-          );
+    const submitBooking = useBookingSubmission({
+        createBooking,
+        editBooking,
+        availableTables,
+        setGuests,
+        setName,
+        setDate,
+        setTime,
+        setIsEditing,
+        setEditingBookingId,
+        hasBookingChanged,
+        restaurantID,
+    });
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            // Call submitBooking with form data, handling both create and edit operations
+            await submitBooking(event, {
+                guests,
+                name,
+                date,
+                time,
+                isEditing,
+                editingBookingId,
+            });
+        } catch (error) {
+            console.error("Failed to process booking:", error);
         }
-      }, [bookings, selectedDate, selectedTimeSlot, availableTables]);
+    };
 
-  const filteredBookingsData = useBookingFilter(
-    bookings,
-    selectedDate,
-    selectedTimeSlot,
-    searchBookings,
-    timeSlotMapping,
-  );
-  const searchFilteredBookings = filteredBookingsData.filter((booking) =>
-    booking.name.toLowerCase().includes(searchBookings.toLowerCase()),
-  );
+    const handleRemoveBooking = async (id) => {
+        await removeBooking(id);
+    };
 
-  return (
-    <div>
-       <div>
-<FilterSection
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-        selectedTimeSlot={selectedTimeSlot}
-        setSelectedTimeSlot={setSelectedTimeSlot}
-        searchBookings={searchBookings}
-        setSearchBookings={setSearchBookings}
-        resetFilters={resetFilters}
-      />
-      </div>
-   
-      <FormManagement
-  guests={guests}
-  setGuests={setGuests}
-  name={name}
-  setName={setName}
-  date={date}
-  setDate={setDate}
-  time={time}
-  setTime={setTime}
-  isEditing={isEditing}
-  handleSubmit={handleSubmit}
-  stopEditing={stopEditing}
-  today={today}
-/>
-     
+    // const handleChangeInSearch = (e) => {
+    //   setSearchBookings(e.target.value);
+    // };
 
-      <h3>Chech Table Availability </h3>
-      <div>{tableAvailabilityMessage && <p>{tableAvailabilityMessage}</p>}</div>
+    const resetFilters = () => {
+        setSelectedDate("");
+        setSelectedTimeSlot("");
+        setSearchBookings("");
+    };
 
+    useEffect(() => {
+        if (selectedDate && selectedTimeSlot) {
+            console.log("Initial bookings:", bookings);
 
+            console.log("Guest Count:", guestCount);
+            console.log("Tables Booked by Customer:", tablesBookedByCustomer);
 
-      <h3>Customer Bookings</h3>
-      <h4>Per selected date and Time:</h4>
-      <h3>Guest Count </h3>
-      {Object.keys(guestCount).map((key) => (
-        <div key={key}>{`${key}: ${guestCount[key]}`}</div>
-      ))}
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        {searchFilteredBookings.map((booking, index) => (
-          <li
-            key={booking.id}
-            style={{
-              borderBottom:
-                index !== filteredBookingsData.length - 1
-                  ? "1px solid #ccc"
-                  : "none",
-            }}
-          >
-            <div>
-              <BookingsListA
+            const totalBookedTables = Object.values(
+                tablesBookedByCustomer
+            ).reduce((acc, cur) => acc + cur, 0);
+            console.log("Total Booked Tables:", totalBookedTables);
+
+            // Log for debugging purposes
+            console.log(
+                "Available Tables (inside useEffect):",
+                availableTables
+            );
+
+            setTableAvailabilityMessage(`Available Tables: ${availableTables}`);
+        } else {
+            setTableAvailabilityMessage(
+                "Please select a date and time to check table availability."
+            );
+        }
+    }, [bookings, selectedDate, selectedTimeSlot, availableTables]);
+
+    const filteredBookingsData = useBookingFilter(
+        bookings,
+        selectedDate,
+        selectedTimeSlot,
+        searchBookings,
+        timeSlotMapping
+    );
+    const searchFilteredBookings = filteredBookingsData.filter((booking) =>
+        booking.name.toLowerCase().includes(searchBookings.toLowerCase())
+    );
+
+    return (
+        <>
+            <FilterSection
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+                selectedTimeSlot={selectedTimeSlot}
+                setSelectedTimeSlot={setSelectedTimeSlot}
+                searchBookings={searchBookings}
+                setSearchBookings={setSearchBookings}
+                resetFilters={resetFilters}
+            />
+
+            <FormManagement
+                guests={guests}
+                setGuests={setGuests}
+                name={name}
+                setName={setName}
+                date={date}
+                setDate={setDate}
+                time={time}
+                setTime={setTime}
+                isEditing={isEditing}
+                handleSubmit={handleSubmit}
+                stopEditing={stopEditing}
+                today={today}
+            />
+            <BookingsListA
                 bookings={searchFilteredBookings}
                 startEditBooking={startEditBooking}
                 handleRemoveBooking={handleRemoveBooking}
                 guestCount={guestCount}
-              />
+            />
+            <div>
+                <h3>Chech Table Availability </h3>
+                <div>
+                    {tableAvailabilityMessage && (
+                        <p>{tableAvailabilityMessage}</p>
+                    )}
+                </div>
+
+                <h3>Customer Bookings</h3>
+                <h4>Per selected date and Time:</h4>
+                <h3>Guest Count </h3>
+                {Object.keys(guestCount).map((key) => (
+                    <div key={key}>{`${key}: ${guestCount[key]}`}</div>
+                ))}
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    {searchFilteredBookings.map((booking, index) => (
+                        <li
+                            key={booking.id}
+                            style={{
+                                borderBottom:
+                                    index !== filteredBookingsData.length - 1
+                                        ? "1px solid #ccc"
+                                        : "none",
+                            }}
+                        ></li>
+                    ))}
+                </ul>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+        </>
+    );
 };
 
 export default AdminInterface;
--- therestaurant/src/components/Admin/FormManagement.jsx
+++ therestaurant/src/components/Admin/FormManagement.jsx
@@ -2,82 +2,86 @@ import Input from "../../UI/Input";
 import { timeSlotMapping } from "../../utils/timeSlot";
 import "./formManagement.scss";
 const FormManagement = ({
-  guests,
-  setGuests,
-  name,
-  setName,
-  date,
-  setDate,
-  time,
-  setTime,
-  isEditing,
-  handleSubmit,
-  stopEditing,
-  today,
+    guests,
+    setGuests,
+    name,
+    setName,
+    date,
+    setDate,
+    time,
+    setTime,
+    isEditing,
+    handleSubmit,
+    stopEditing,
+    today,
 }) => {
-  return (
-    <div className="form-management">
-      <h1>{isEditing ? "Edit Booking" : "Create Booking"}</h1>
-      <form onSubmit={handleSubmit}>
-        <Input
-          label="Date of Arrival"
-          type="date"
-          min={today}
-          value={date}
-          className="input-field" // Added class for styling
-          onChange={(e) => setDate(e.target.value)}
-          required
-        />
-        <div>
-          <label htmlFor="timeSlot">Time Slot</label>
-          <select
-            name="timeSlot"
-            className="input-field" // Use the same class for consistent styling
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-          >
-            {Object.keys(timeSlotMapping).map((slot) => (
-              <option key={slot} value={slot}>
-                {slot}
-              </option>
-            ))}
-          </select>
+    return (
+        <div className="form-management">
+            <h1>{isEditing ? "Edit Booking" : "Create Booking"}</h1>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    label="Date of Arrival"
+                    type="date"
+                    min={today}
+                    value={date}
+                    className="input-field" // Added class for styling
+                    onChange={(e) => setDate(e.target.value)}
+                    required
+                />
+                <div>
+                    <label htmlFor="timeSlot">Time Slot</label>
+                    <select
+                        name="timeSlot"
+                        className="input-field" // Use the same class for consistent styling
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                    >
+                        {Object.keys(timeSlotMapping).map((slot) => (
+                            <option key={slot} value={slot}>
+                                {slot}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <Input
+                    label="Number of Guests"
+                    type="number"
+                    value={guests}
+                    className="input-field" // Added class for styling
+                    onChange={(e) => setGuests(e.target.value)}
+                    min="1"
+                    step="1"
+                    required
+                />
+                <Input
+                    label="Customer Name"
+                    type="text"
+                    value={name}
+                    className="input-field" // Added class for styling
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <button type="submit" className="submit-button">
+                    {isEditing ? "Save Changes" : "Confirm Booking"}
+                </button>
+                <button
+                    type="button"
+                    className="cancel-button" // Use specific class for the cancel button
+                    onClick={() => {
+                        if (
+                            window.confirm(
+                                "Are you sure you want to clear the form?"
+                            )
+                        ) {
+                            stopEditing();
+                        }
+                    }}
+                >
+                    Clear
+                </button>
+            </form>
         </div>
-        <Input
-          label="Number of Guests"
-          type="number"
-          value={guests}
-          className="input-field" // Added class for styling
-          onChange={(e) => setGuests(e.target.value)}
-          min="1"
-          step="1"
-          required
-        />
-        <Input
-          label="Customer Name"
-          type="text"
-          value={name}
-          className="input-field" // Added class for styling
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <button type="submit" className="submit-button">
-          {isEditing ? "Save Changes" : "Confirm Booking"}
-        </button>
-        <button
-          type="button"
-          className="cancel-button" // Use specific class for the cancel button
-          onClick={() => {
-            if (window.confirm("Are you sure you want to clear the form?")) {
-              stopEditing();
-            }
-          }}
-        >
-          Clear
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default FormManagement;
